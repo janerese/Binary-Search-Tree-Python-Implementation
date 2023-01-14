0a1ab3093f8e6cb900431da81309869f4be364ec
Erika Jane Reyes <erikajanetreyes@iskolarngbayan.pup.edu.ph>
2023-01-14 18:38:21 +0800

    Defined binary search tree Class

---
+++ binary_tree.py
@@ -0,0 +1,8 @@
+# Erika Jane T. Reyes
+
+# Defining binary search tree Class (BinarySearchTreeNode)
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
